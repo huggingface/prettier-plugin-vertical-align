ef8833147daf4f93a12c4a09a51d7d18cd6279cc
coyotte508 <coyotte508@protonmail.com>
2024-10-16 15:42:52 +0200

    fix crash

---
+++ packages/test/src/should-parse.ts
@@ -0,0 +1,12 @@
+// We do not want to write this file, only check that the parser can handle it.
+//
+// As it will remove the parentheses, the `loc` information will be lost in  the AST on some nodes.
+
+const xy = {
+  firstVar:
+			!!repoStatus?.bo?.someVar &&
+			!!repoStatus?.someStuff &&
+			(showsomeStuff &&
+			repoStatus.someStuff.someVaar100 !== BLABLAxxxxx.xxxoxoxox),
+		secondProperty: [...boooMooo.entries()].map(([path, level]) => ({ path, level })),
+}
